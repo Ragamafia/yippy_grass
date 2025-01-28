19d2ba52f64eeed5c02f8b4bbf493f7be5424079
Иван Тоцких <sales3-suzuki@terra.local>
2025-01-28 11:54:21 +0800

    refactoring from 9ft6

--- src/client.py
+++ src/client.py
@@ -1,132 +1,81 @@
+import asyncio
 import json
 import random
-import uuid
-import time
 
 from loguru import logger
 from aiohttp import ClientSession
 
+from proxies.pool import get_proxy
+from base import user_devices
 from config import cfg
 
 
 SOCKET_URL = random.choice(cfg.urls)
 
-auth = {
-    "id": "",
-    "origin_action": "AUTH",
-    "result": {
-        "browser_id": "",
-        "user_id": "",
-        #"user_agent": "",
-        "timestamp": 1736645161,
-        "device_type": "extension",
-        "version": "4.26.2",
-        "extension_id": "ilehaonighjijnmpnagapkhpcdbhclfg"
-    }
-        }
-
-http_request = {
-    "id": "",
-    "action": "HTTP_REQUEST",
-    "data": {
-        "url": "",
-        "method": "GET",
-        "headers": {},
-    },
-    "body": None,
-    "authenticated": False
-}
-
-ping = {"id": "", "version": "1.0.0", "action": "PING", "data": {}}
-
-pong = {"id": "", "origin_action": "PONG"}
-
-
-class Connect:
-    session: ClientSession
-    proxy: str
-    user_agent: dict
-    auth: dict
-    http_request: dict
-    ping: dict
-    pong: dict
-
-    def __init__(self, session, user_agent, proxy):
-        self.session = session
-        self.user_agent = user_agent
-        self.proxy = proxy
-
-    async def connect_to_ws(self):
-        connected = False
-
-        kwargs = {
-
-            "headers": {
-                "User-Agent": self.user_agent.get("user_agent"),
-                "Accept": "application/json",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
+async def parse_message(session: ClientSession, message: dict, device: dict, proxy:str, headers: dict):
+    if message.get('action') == "AUTH":
+        return {
+            "id": "",
+            "origin_action": "AUTH",
+            "result": {
+                "browser_id": device["device_id"],
+                "user_id": device["user_id"],
+                "user_agent": device["user_agent"],
+                "timestamp": 1736645161,
+                "device_type": "extension",
+                "version": "4.26.2",
+                "extension_id": "ilehaonighjijnmpnagapkhpcdbhclfg"
             }
         }
 
-        try:
-            async with self.session.ws_connect(SOCKET_URL, proxy=self.proxy, **kwargs) as self.ws:
-                async for message in self.ws:
+    if message.get('action') == "PONG":
+        return {"id": "", "origin_action": "PONG"}
 
-                    message_data = message.__getattribute__('data')
-                    self.data = json.loads(message_data)
-                    logger.info(f"<- Received: {self.data}")
+    if message.get('action') == "HTTP_REQUEST":
+        url = message.get("data").get("url")
+        async with session.get(url, proxy=proxy, headers=headers) as response:
+            body = await response.text()
+            print(body)
+            return {
+                "id": "",
+                "action": "HTTP_REQUEST",
+                "data": {
+                    "url": url,
+                    "method": "GET",
+                    "headers": response.headers,
+                },
+                "body": body,
+                "authenticated": False
+            }
 
-                    if self.data.get('action') == "AUTH":
-                        await self.send_auth()
 
-                    elif self.data.get('action') == "HTTP_REQUEST":
-                        url = self.data.get("data").get("url")
-                        async with self.session.get(url, **kwargs) as response:
-                            body = await response.text()
-                            print(body)
+async def run_device(device):
+    headers = {
+        "User-Agent": device.get("user_agent"),
+        "Accept": "application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
+    }
+    kwargs = {
+        "trust_env":  True,
+        "headers": headers
+    }
+    proxy = await get_proxy()
+    async with ClientSession(**kwargs) as session:
+        logger.info(f'Connecting WS with proxy: {proxy.url}')
+        async with session.ws_connect(SOCKET_URL) as ws:
+            async for message in ws:
+                message = json.loads(message.data)
+                logger.success(f"<- Received: {message}")
+                if response := await parse_message(session, message, device, proxy.url, headers):
+                    response["id"] = message["id"]
+                    await ws.send_json(response)
 
-                        # if await self.send_http_request():
-                        #     await self.send_ping()
 
-                    elif self.data.get('action') == "PONG":
-                        await self.send_pong()
+async def main():
+    devices = user_devices.get_devices(cfg.device_count)
+    await asyncio.gather(*[run_device(d) for d in devices])
 
-                    connected = True
-            return connected
 
-        except Exception as e:
-            logger.error(f"<- Connect error: {e} - {self.data}")
-
-    async def send_auth(self):
-        auth["id"] = self.data.get("id")
-        auth["result"]["browser_id"] = self.user_agent.get("device_id")
-        auth["result"]["user_id"] = self.user_agent.get("user_id")
-        auth["result"]["user_agent"] = self.user_agent.get("user_agent")
-
-        await self.ws.send_json(auth)
-        logger.info(f" -> Sending: {auth}")
-
-    async def send_http_request(self):
-        http_request["id"] = self.data.get("id")
-        http_request["data"]["url"] = self.data.get("data").get("url")
-        http_request["data"]["headers"] = self.user_agent
-
-        await self.ws.send_json(http_request)
-        logger.info(f"-> Sending: {http_request}")
-        return True
-
-    async def send_ping(self):
-        ping["id"] = str(uuid.uuid3(uuid.NAMESPACE_DNS, SOCKET_URL))
-        await self.ws.send_json(ping)
-        logger.info(f"-> Sending: {ping}")
-
-    async def send_pong(self):
-        pong["id"] = self.data.get("id")
-        ping["id"] = str(uuid.uuid4())
-        await self.ws.send_json(pong)
-        logger.info(f'-> Sending: {pong}')
-
-        time.sleep(cfg.request_time_sleep)
-        await self.ws.send_json(ping)
-        logger.info(f"-> Sending: {ping}")
+if __name__ == "__main__":
+    asyncio.run(main())
